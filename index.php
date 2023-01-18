b2546da01745ecc29cd182cd69ad6bf58b3d602d
Elizabeth Villarruel Rosales <108765164+ellyhp@users.noreply.github.com>
2023-01-17 18:59:50 -0600

    v1

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Formailario por GET</title>
+</head>
+<body>
+    
+    <form action="server.php" method="post" enctype=multipart/form-data>
+
+    
+    <label for="image">Imagen</label>
+    <input type="file" name="image" id="image">
+
+
+        <button type="submit">></button>
+
+    </form>
+
+</body>
+</html>
